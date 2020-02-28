be7a28beb1dba8d90a92f933a1101a0847669743
Lindsay Allen <lindsay.allen@wsu.edu>
2020-02-28 13:12:08 -0800

    navbar styled

--- portfolio/src/components/Menubar.js
+++ portfolio/src/components/Menubar.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import AppMode from '../AppModes';
 import '../style/Menubar.css';
+import Navbar from 'react-bootstrap/Navbar';
+
+
 
 class Menubar extends React.Component {
 
     render() {
         return(
-            <div>
+            <div className="Navbar">
                 <a onClick={() => this.props.changeMode(AppMode.AboutMe)}>About Me</a>
                 <a onClick={() => this.props.changeMode(AppMode.Experience)}>Experience</a>
                 <a onClick={() => this.props.changeMode(AppMode.Projects)}>Projects</a>
