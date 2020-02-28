358fe5f1d413ee44c92321a83ee4d782ef60dbff
Lindsay Allen <lindsay.allen@wsu.edu>
2020-02-28 13:37:38 -0800

    started text portion

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Navbar from './components/Navbar';
-import About from './components/AboutMe';
+import About from './components/About';
 import Projects from './components/Projects';
 import Experience from './components/Experience';
 import Contact from './components/Contact';
---
+++ portfolio/src/components/About.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../style/About.css';
+
+class About extends React.Component {
+
+    render() {
+        return(
+            <div>
+                I am expected to graduate with a BS in Computer Science from Washington State University in December of 2020. I initially began my degree path in Neuroscience, but an intro
+                to programming class was all I needed to see this was my true passion. I've had the opportunity to intern in the Research & Development department at Schweitzer Engineering 
+                Laboratories where I was exposed to all different types of programming from microcontrollers to web development. On my free time I play guitar, ride motorcycles, and hang 
+                out with the coolest pup in the world.
+            </div>
+        );
+    }
+
+}
+
+export default About;
