a61578d68a1765c522f609100b670b9d5c1094f6
Lindsay Allen <lindsay.allen@wsu.edu>
2020-02-28 15:49:27 -0800

    added styled project templates

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -24,7 +24,7 @@ class App extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      mode: AppMode.Contact
+      mode: AppMode.Projects
     }
   }
 
--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -5,8 +5,54 @@ class Projects extends React.Component {
 
     render() {
         return(
-            <div>
-                ON PROJECTS PAGE
+            <div className="full-wrap">
+                <div style={{paddingTop: "20px"}}></div>
+               <h3>Professional</h3>
+               <p></p>
+                    <div className="project">
+                        <div className="title">Title</div>
+                        <div className="tools">Languages, OS, frameworks, or libraries used</div>
+                        <div className="summary">Summary of project</div>
+                        <div className="link">**unable to provide code samples due to confidentiality agreement</div>
+                        <div className="line"></div>
+                    </div>
+                    <p></p>
+               <h3>Private</h3>
+               <p></p>
+                    <div className="project">
+                        <div className="title">Title</div>
+                        <div className="tools">Languages, OS, frameworks, or libraries used</div>
+                        <div className="summary">Summary of project</div>
+                        <div className="link">link to code if it exists</div>
+                        <div className="line"></div>
+                    </div>
+                    <p></p>
+                    <div className="project">
+                        <div className="title">Title</div>
+                        <div className="tools">Languages, OS, frameworks, or libraries used</div>
+                        <div className="summary">Summary of project</div>
+                        <div className="link">link to code if it exists</div>
+                        <div className="line"></div>
+                    </div>
+                    <p></p>
+               <h3>Academic</h3>
+               <p></p>
+                    <div className="project">
+                        <div className="title">Title</div>
+                        <div className="tools">Languages, OS, frameworks, or libraries used</div>
+                        <div className="summary">Summary of project</div>
+                        <div className="link">link to code if it exists</div>
+                        <div className="line"></div>
+                    </div>
+                    <p></p>
+                    <div className="project">
+                        <div className="title">Title</div>
+                        <div className="tools">Languages, OS, frameworks, or libraries used</div>
+                        <div className="summary">Summary of project</div>
+                        <div className="link">link to code if it exists</div>
+                        <div className="line"></div>
+                    </div>
+                    <p></p>
             </div>
         );
     }
