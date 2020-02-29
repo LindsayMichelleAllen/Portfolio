a3da1dc48fdec4ae0b4bd13cb126e59cc991a46b
Lindsay Allen <lindsay.allen@wsu.edu>
2020-02-29 09:36:50 -0800

    content complete

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -24,7 +24,7 @@ class App extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      mode: AppMode.Experience
+      mode: AppMode.AboutMe
     }
   }
 
--- portfolio/src/components/Navbar.js
+++ portfolio/src/components/Navbar.js
@@ -9,6 +9,7 @@ class Navbar extends React.Component {
     render() {
         return(
             <div className="Navbar">
+                <div className="name">Lindsay Allen - Software Engineer</div>
                 <a className='icon' onClick={() => this.props.changeMode(AppMode.AboutMe)}>About Me</a>
                 <a className='icon' onClick={() => this.props.changeMode(AppMode.Experience)}>Experience</a>
                 <a className='icon' onClick={() => this.props.changeMode(AppMode.Projects)}>Projects</a>
